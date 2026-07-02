62574dd07f124879c1bb8719bdb5b8059d15eab6
hussainmohd-a <hussainmohd.a@gmail.com>
2026-07-03 00:21:19 +0530

    dns: rmv uidSelf usage, use start ms sent in querytime calc

--- app/src/main/java/com/celzero/bravedns/service/DnsLogTracker.kt
+++ app/src/main/java/com/celzero/bravedns/service/DnsLogTracker.kt
@@ -74,20 +74,14 @@ internal constructor(
         vpnStateMap[Transaction.Status.INTERNAL_ERROR] = BraveVPNService.State.APP_ERROR
     }
 
-    fun processOnResponse(summary: DNSSummary, rethinkUid: Int): Transaction {
-        val latencyMs = summary.start
+    fun processOnResponse(summary: DNSSummary): Transaction {
+        val latencyMs = (TimeUnit.SECONDS.toMillis(1L) * summary.latency).toLong()
         val nowMs = SystemClock.elapsedRealtime()
-        val queryTimeMs = nowMs - latencyMs
+        val queryTimeMs = nowMs - summary.start
         var uid = INVALID_UID
 
         try {
-            uid = if (summary.uid == Backend.UidSelf) {
-                rethinkUid
-            } else if (summary.uid == Backend.UidSystem) {
-                AndroidUidConfig.SYSTEM.uid // 1000
-            } else {
-                summary.uid.toInt()
-            }
+            uid = summary.uid.toInt()
         } catch (_: NumberFormatException) {
             Logger.w(LOG_TAG_VPN, "onQuery: invalid uid: ${summary.uid}, using default uid: $uid")
         }
